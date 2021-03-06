b446c23ccb733b868a6f610fd26a1922bf5b7100
Jimmy Escalante <escalasa@oregonstate.edu>
2021-03-05 22:22:20 -0600

    Implemented required user stories, refresh, and infinite load.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -9,6 +9,74 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweets: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+
+    
+    override func viewDidLoad() {
+        //load the view
+        super.viewDidLoad()
+        //load the tweet
+        loadTweets()
+        //refresh
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    @objc func loadTweets(){
+        numberOfTweets = 20
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweets]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            //clear array
+            self.tweetArray.removeAll()
+            //repopulate array with tweets from timeline
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+            }
+            //update table
+            self.tableView.reloadData()
+            //end refresh
+            self.myRefreshControl.endRefreshing()
+        }, failure: { (Error) in
+            print("Could not load tweet!")
+        })
+    }
+    
+    
+    func loadMoreTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        
+        numberOfTweets = numberOfTweets + 20
+        let myParams = ["count": numberOfTweets]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            //clear array
+            self.tweetArray.removeAll()
+            //repopulate array with tweets from timeline
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+            }
+            //update table
+            self.tableView.reloadData()
+            //end refresh
+            self.myRefreshControl.endRefreshing()
+        }, failure: { (Error) in
+            print("Could not load tweet!")
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count{
+            loadMoreTweets()
+        }
+    }
+    
 
     @IBAction func onLogout(_ sender: Any) {
         //logout call to API
@@ -20,21 +88,30 @@ class HomeTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetCellTableViewCell
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        //Display content from Twitter API
+        cell.userNameLabel.text = user["name"] as! String
+        cell.TweetContent.text = tweetArray[indexPath.row]["text"] as! String
+        
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data{
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
         return cell
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+ 
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 5
+        // number of rows
+        return tweetArray.count
     }
 
    
