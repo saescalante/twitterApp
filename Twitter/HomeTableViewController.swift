3f524b93e1cb60afd84461446ca1e3d51ec5de03
Jimmy Escalante <escalasa@oregonstate.edu>
2021-03-12 23:19:02 -0600

    Favoriting and retweeting user stories implemented.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -23,6 +23,8 @@ class HomeTableViewController: UITableViewController {
         //refresh
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -106,6 +108,12 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeded(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+        
         
         return cell
     }
--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -11,7 +11,7 @@ class TweetViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tweetTextView.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
     
